234541882306613eb61220c37f6cea10c1d02f79
Stuart Mumford <stuart@cadair.com>
2025-03-05 09:20:11 +0000

    Auto tangle in Git/new.emacs.d

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (defun start/org-babel-tangle-config ()
   "Automatically tangle our Emacs.org config file when we save it. Credit to Emacs From Scratch for this one!"
   (when (string-equal (file-name-directory (buffer-file-name))
-                      (expand-file-name user-emacs-directory))
+                      (expand-file-name "~/Git/new.emacs.d/"))
     ;; Dynamic scoping to the rescue
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
